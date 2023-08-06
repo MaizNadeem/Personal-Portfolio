64c14f7cd7e0bc57513eec3e123331e2daec7a88
MaizNadeem <maiznadeem616@gmail.com>
2023-08-07 02:56:45 +0500

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Navbar from './components/NavigationBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            
+            <Navbar />
         </div>
     );
 }
