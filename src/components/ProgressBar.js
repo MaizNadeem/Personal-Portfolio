444bcb6882c0467754d8b3b91d4cc2f0b0a26e6a
MaizNadeem <maiznadeem616@gmail.com>
2023-08-08 00:24:46 +0500

    Carousel Fixed

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,9 +1,11 @@
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+
+import 'react-circular-progressbar/dist/styles.css';
 
 const ProgressBar = ({ percentage }) => {
+    
     const [isVisible, setIsVisible] = useState(false);
     const [currentPercentage, setCurrentPercentage] = useState(0);
 
@@ -43,42 +45,17 @@ const ProgressBar = ({ percentage }) => {
             }}
             ref={ref}
         >
-            <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-                <CircularProgressbar
-                    value={currentPercentage}
-                    text={`${Math.round(currentPercentage)}%`}
-                    styles={{
-                        path: {
-                            stroke: 'transparent',
-                        },
-                        trail: {
-                            stroke: 'transparent',
-                        },
-                        text: {
-                            fill: "white",
-                        },
-                    }}
-                />
-                <svg width="100%" height="100%" viewBox="0 0 100 100" style={{ position: 'absolute', top: 0, left: 0 }}>
-                    <defs>
-                        <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                            <stop offset="0%" stopColor="#9F3483" />
-                            <stop offset="100%" stopColor="#5933B4" />
-                        </linearGradient>
-                    </defs>
-                    <circle
-                        cx="50"
-                        cy="50"
-                        r="47"
-                        fill="none"
-                        strokeWidth="6"
-                        stroke="url(#gradient)"
-                        strokeDasharray={`${currentPercentage}, 100`}
-                        strokeLinecap="round"
-                        transform="rotate(0 50 50)"
-                    />
-                </svg>
-            </div>
+            <CircularProgressbar
+                value={currentPercentage}
+                text={`${Math.round(currentPercentage)}%`}
+                styles={buildStyles({
+                    pathColor: '#9F3483',
+                    trailColor: 'transparent',
+                    textColor: 'white',
+                    textSize: '18px',
+                    rotation: 0.25,
+                })}
+            />
         </div>
     );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,16 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
+
+import Carousel from 'react-multi-carousel';
+
+
 import ProgressBar from "./ProgressBar";
+
+import 'react-multi-carousel/lib/styles.css';
 
 const Skills = () => {
 
@@ -17,11 +21,11 @@ const Skills = () => {
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3
+            items: 4
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2
+            items: 3
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -37,7 +41,15 @@ const Skills = () => {
                         <div className="skill-bx wow zoomIn">
                             <h2>Skills</h2>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="gd-carousel-wrapper">
+                            <Carousel 
+                                swipeable={true}
+                                draggable={true}
+                                arrows={true}
+                                responsive={responsive}
+                                infinite={false}
+                                showDots={true}
+                                className="gd-carousel">
                                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: '1rem' }} >
                                     <ProgressBar percentage={95} />
                                     <h5>Web Development</h5>
@@ -61,6 +73,7 @@ const Skills = () => {
                                     </div>
                                 </div>
                             </Carousel>
+                            </div>
                         </div>
                     </div>
                 </div>
